1c68cd55ef4a5a8b80d9ed74788db1b50eec3e94
Wolff Junior <wolfflobo2@gmail.com>
2021-08-16 17:54:12 -0300

    add comentario.js

---
+++ comentario.js
@@ -0,0 +1,16 @@
+// Comentario de uma linha
+console.log('Linha 1')
+
+
+
+/*
+comentario de
+multiplas linha
+*/
+console.log('Linha 2')
+
+/*
+comentario de
+multiplas linha
+*/
+console.log('Linha 3')
